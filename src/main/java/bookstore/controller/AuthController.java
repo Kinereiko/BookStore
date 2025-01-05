0d93bc6d0c28fd723bab5168fdb7d68568cfb9d4
Kinereiko <kinereiko@gmail.com>
2025-01-05 21:21:11 +0200

    Add @Valid annotation

--- src/main/java/bookstore/controller/AuthController.java
+++ src/main/java/bookstore/controller/AuthController.java
@@ -27,7 +27,7 @@ public class AuthController {
 
     @PostMapping("/login")
     @Operation(summary = "Login", description = "Login user and return token")
-    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto requestDto) {
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
         return authenticationService.authenticate(requestDto);
     }
 
